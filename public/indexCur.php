10e41fe0b127aba5fcd1097ae42d4fc8754b2d0b
alanmanderson <alanmanderson@gmail.com>
2017-08-10 10:29:50 -0600

    Update to Laravel 5.4

--- public/indexCur.php
+++
@@ -1,7 +0,0 @@
-<?php
-#$to = "alanmanderson@gmail.com";
-#$subject = "Tuesday night bball";
-#$message = "click http://alanmanderson.com/RedStake/bball/confirm.php to accept";
-
-#echo mail($to, $subject, $message);
-echo "Hello head_count";
