eee8a987155fc5f86d576d8e7b97cb353b5d58e3
Alan Anderson <alanmanderson@gmail.com>
2018-07-03 21:15:49 -0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,10 @@
 <?php
 
+use Alanmanderson\HeadCount\Models\Occurrence;
+use Alanmanderson\HeadCount\Notifications\EventRsvp;
+use Cron\CronExpression;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Notifications\Notification;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,18 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('occurrence:create {event_id}', function(int $event_id) {
+    $e = Event::find($event_id);
+    $cron = CronExpression::factory($e->schedule);
+    $occurrence = Occurrence::create([
+        'event_id' => $e->id,
+        'start_time' => $cron->getNextRunDate()->format('Y-m-d H:i:s')
+    ]);
+    $this->info($occurrence);
+})->describe('Create an instance of an event');
+
+Artisan::command('users:notify {occurrence_id}', function(int $occurrence_id) {
+    $occurrence = Occurrence::find($occurrence_id);
+    Notification::send($occurrence->event->users, new EventRsvp($occurrence));
+})->describe('Notify all users for the given occurence.');
